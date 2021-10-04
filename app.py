dd93d4099a09cc7df3135c3feee75eb360cc72e1
John Loane <john.loane@dkit.ie>
2021-10-04 13:27:28 +0100

    Making the application persistant with MysqlDB
    Make sure you pip install flask-mysqldb for this project

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
+mysql = MySQL(app)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = 'sd3a_music_festival'
 
 MUSIC_FESTIVALS = [
-    "Electic Picnic",
+    "Electricpicnic",
     "Longitude",
     "Tomorrowland",
     "Reading"
 ]
 
+REGISTRANTS = {}
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", festivals = MUSIC_FESTIVALS)
 
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("musicfestival"):
+    name = request.form.get("name")
+    festival_chosen = request.form.get("musicfestival")
+    print(festival_chosen)
+    if not name or not festival_chosen or festival_chosen not in MUSIC_FESTIVALS:
         return render_template("failure.html")
-    return render_template("success.html")
+    REGISTRANTS[name] = festival_chosen
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO registrant(name, festival) VALUES (%s, %s)", (name, festival_chosen))
+    mysql.connection.commit()
+    cur.close()
+    return redirect("/registrants")
+
+
+@app.route("/registrants")
+def registrants():
+    return render_template("registrants.html", registrants = REGISTRANTS)
 
 
 app.run(debug=True)
