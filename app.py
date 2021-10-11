8f7d2b5ac5d9bba7e24e4166b4b1f2bc8dae2c22
John Loane <john.loane@dkit.ie>
2021-10-11 17:12:48 +0100

    Example to show how to use Mysql with flask

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
+from flask_mail import Mail, Message
 import os
 
 app = Flask(__name__)
@@ -7,8 +8,20 @@ mysql = MySQL(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sd3a_music_festival'
+
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+
+mail = Mail(app)
+
+
+
 
 MUSIC_FESTIVALS = [
     "Electricpicnic",
@@ -27,22 +40,28 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    name = request.form.get("name")
+    email = request.form.get("email")
     festival_chosen = request.form.get("musicfestival")
+    print(email)
     print(festival_chosen)
-    if not name or not festival_chosen or festival_chosen not in MUSIC_FESTIVALS:
+    if not email or not festival_chosen or festival_chosen not in MUSIC_FESTIVALS:
         return render_template("failure.html")
-    REGISTRANTS[name] = festival_chosen
+    REGISTRANTS[email] = festival_chosen
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO registrant(name, festival) VALUES (%s, %s)", (name, festival_chosen))
+    cur.execute("INSERT INTO registrant(name, festival) VALUES (%s, %s)", (email, festival_chosen))
     mysql.connection.commit()
     cur.close()
+    message = Message("You are registered", recipients=[email])
+    mail.send(message)
     return redirect("/registrants")
 
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants = REGISTRANTS)
+    cur = mysql.connection.cursor()
+    cur.execute("select name, festival from registrant")
+    registrants = cur.fetchall()
+    return render_template("registrants.html", registrants = registrants)
 
 
 app.run(debug=True)
